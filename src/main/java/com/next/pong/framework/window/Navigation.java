a2e527c0afa5cfacc4c846b03c530a2e04ee3f28
Avon FitzGerald <avon.fitzy@gmail.com>
2022-10-12 13:24:15 +0200

    Added Navigation. Switch between activities

---
+++ src/main/java/com/next/pong/framework/window/Navigation.java
@@ -0,0 +1,40 @@
+package com.next.pong.framework.window;
+
+import com.next.pong.framework.activity.Activity;
+
+class Navigation {
+
+    private static Activity currentActivity = null;
+    private static OnActivityChangeListener onActivityChangeListener = null;
+
+    interface OnActivityChangeListener {
+        void onChange(Activity oldActivity, Activity newActivity);
+    }
+
+    protected static void goTo(Activity activity) {
+
+        if (currentActivity != null) {
+            currentActivity.onStop();
+        }
+
+        if (onActivityChangeListener != null) {
+            onActivityChangeListener.onChange(currentActivity, activity);
+        }
+
+        currentActivity = activity;
+    }
+
+    protected static void update(float deltaTime) {
+
+        if (currentActivity == null) {
+            return;
+        }
+
+        currentActivity.onUpdate(deltaTime);
+    }
+
+    protected static void setOnActivityChangeListener(OnActivityChangeListener listener) {
+        onActivityChangeListener = listener;
+    }
+
+}
