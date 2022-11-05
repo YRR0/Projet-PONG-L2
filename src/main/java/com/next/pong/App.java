a09f894b596f0895453d372c8c0ffd200a9f8ecb
Avon FitzGerald <avon.fitzy@gmail.com>
2022-11-06 00:37:35 +0100

    Reverting attempt at pausing in Window

--- src/main/java/com/next/pong/App.java
+++ src/main/java/com/next/pong/App.java
@@ -4,11 +4,12 @@ import com.next.pong.framework.window.Window;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class App extends Application {
+
     @Override
     public void start(Stage primaryStage) {
         Window.init(primaryStage);
         Window.startLoop();
     }
+
 }
