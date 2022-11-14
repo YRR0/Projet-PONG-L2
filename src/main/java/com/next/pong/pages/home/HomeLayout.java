52d3b7ab09348688aef924f5cfd2946951597143
Deniz Ertekin <KingDen01@yandex.ru>
2022-11-14 15:13:20 +0100

    c'est test menu (on va utiliser cette version de menu)

--- src/main/java/com/next/pong/pages/home/HomeLayout.java
+++ src/main/java/com/next/pong/pages/home/HomeLayout.java
@@ -1,76 +1,12 @@
 package com.next.pong.pages.home;
 
 import com.next.pong.framework.layout.Layout;
-import com.sun.javafx.geom.Rectangle;
-import javafx.animation.Animation;
-import javafx.animation.FadeTransition;
-import javafx.animation.FillTransition;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.geometry.Pos;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
-
-
-import java.util.Collection;
-
-import static javafx.scene.paint.Color.LIGHTBLUE;
 
 
 public abstract class HomeLayout extends Layout {
 
     public HomeLayout() {
-        Pane root = new Pane();
-        MenuItem newGame = new MenuItem("New game");
-        MenuItem options = new MenuItem("Settings");
-        MenuItem exitGame = new MenuItem("Quit");
-        SubMenu mainMenu = new SubMenu(newGame, options, exitGame);
-        MenuItem sound = new MenuItem("Sound");
-        MenuItem video = new MenuItem("Video");
-        MenuItem keys = new MenuItem("Control");
-        MenuItem optionsBack = new MenuItem("Back");
-        SubMenu optionsMenu = new SubMenu(sound, video, keys, optionsBack);
-        MenuBox menuBox = new MenuBox(mainMenu);
-    }
-
-    public void start(Stage primaryStage) {
-        newGame.setOnMouseClicked(event -> menuBox.setSubMenu(newGameMenu));
-        options.setOnMouseClicked(event -> menuBox.setSubMenu(optionsMenu));
-        exitGame.setOnMouseClicked(event -> System.exit(0));
-        optionsBack.setOnMouseClicked(event -> menuBox.setSubMenu(mainMenu));
-        root.getChildren().addAll(menuBox);
-
-        Scene scene = new Scene(root, 900, 600);
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ESCAPE) {
-                FadeTransition ft = new FadeTransition(Duration.seconds(1), menuBox);
-                if (!menuBox.isVisible()) {
-                    ft.setFromValue(0);
-                    ft.setToValue(1);
-                    ft.play();
-                    menuBox.setVisible(true);
-                } else {
-                    ft.setFromValue(1);
-                    ft.setToValue(0);
-                    ft.setOnFinished(evt -> menuBox.setVisible(false));
-                    ft.play();
-
-                }
-            }
-        });
-        primaryStage.setTitle("Pause");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        super();
     }
 
 }
---
+++ src/main/java/com/next/pong/pages/home/MainMeunController.java
@@ -0,0 +1,60 @@
+/**
+ * Sample Skeleton for 'MainMeun.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="historyOfGame"
+    private Button historyOfGame; // Value injected by FXMLLoader
+
+    @FXML // fx:id="newGameButton"
+    private Button newGameButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="quitTheGame"
+    private Button quitTheGame; // Value injected by FXMLLoader
+
+    @FXML // fx:id="settingsOfGame"
+    private Button settingsOfGame; // Value injected by FXMLLoader
+
+    @FXML
+    void configuration(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newGameAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showHistoryOfGame(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert historyOfGame != null : "fx:id=\"historyOfGame\" was not injected: check your FXML file 'MainMeun.fxml'.";
+        assert newGameButton != null : "fx:id=\"newGameButton\" was not injected: check your FXML file 'MainMeun.fxml'.";
+        assert quitTheGame != null : "fx:id=\"quitTheGame\" was not injected: check your FXML file 'MainMeun.fxml'.";
+        assert settingsOfGame != null : "fx:id=\"settingsOfGame\" was not injected: check your FXML file 'MainMeun.fxml'.";
+
+    }
+
+}
