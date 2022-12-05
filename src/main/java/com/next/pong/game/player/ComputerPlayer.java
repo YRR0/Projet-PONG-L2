572c9d9d6978ca9bab721c2503e941800c206519
Avon FitzGerald <avon.fitzy@gmail.com>
2022-12-05 17:27:34 +0100

    Cleaned up AI code

--- src/main/java/com/next/pong/game/player/ComputerPlayer.java
+++ src/main/java/com/next/pong/game/player/ComputerPlayer.java
@@ -7,32 +7,28 @@ import com.next.pong.utils.Vector2;
 public class ComputerPlayer extends Player {
 
     private Ball ball;
+
     public ComputerPlayer(Vector2 position, Vector2 speed, Vector2 size, Ball ball) {
         super(position, speed, size);
         this.ball = ball;
-        keyEventListener = null;
-    }
-
-    private void moveUp() {
-        applyForceUp();
-    }
-
-    private void moveDown() {
-        applyForceDown();
     }
 
     public void move() {
-        Vector2 RacketPosition = this.getPosition();
-        Vector2 BallPosition   = ball.getPosition();
-        double screenMid = Layout.DEFAULT_WIDTH / 2;
-        if((RacketPosition.x() > screenMid && BallPosition.x() > screenMid) || (RacketPosition.x() < screenMid && BallPosition.x() < screenMid)) {
-            if(BallPosition.y() < RacketPosition.y()) {
-                moveUp();
-            } else if(BallPosition.y() > RacketPosition.y()) {
-                moveDown();
+        Vector2 RacketPosition = getPosition();
+        Vector2 BallPosition = ball.getPosition();
+
+        double screenMid = 0.5 * Layout.DEFAULT_WIDTH;
+
+        boolean isSameLeftSide = RacketPosition.x() < screenMid && BallPosition.x() < screenMid;
+        boolean isSameRightSide = RacketPosition.x() > screenMid && BallPosition.x() > screenMid;
+
+        if (isSameLeftSide || isSameRightSide) {
+            if (BallPosition.y() < RacketPosition.y()) {
+                applyForceUp();
+            } else if (BallPosition.y() > RacketPosition.y()) {
+                applyForceDown();
             }
         }
-
     }
 
 }
